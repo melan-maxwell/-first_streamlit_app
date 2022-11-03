ca171e29b09c9054387cd58f938e75875b24233d
melan-maxwell <117357690+melan-maxwell@users.noreply.github.com>
2022-11-03 17:14:23 +0530

    Update streamlit_app.py
    Added menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('my parents new healthy dinner')
+streamlit.header('Breakfast menu')
+streamlit.text('omega 3 and blue berry oatmeal')
+streamlit.text('Spinach and smoothy')
+streamlit.text('Boiled egg')
